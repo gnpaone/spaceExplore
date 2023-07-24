54c72a37376f077a43c684223fcd4fda9cc6f84a
Naveen Prashanth <78990165+gnpaone@users.noreply.github.com>
2023-07-24 18:47:07 +0530

    Delete pages directory

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import Main from '../planets/Main'
-
-function MyApp({ Component, pageProps }) {
-  return <Component canvas={<Main />} {...pageProps} />
-}
-
-export default MyApp
